683fb668817f5dd9abb548a348b04a6f3178e7b3
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-05 20:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function sub(a, b) {
   return (b - a);
 }
 
+function multiply() {
+  return (a * b);
+}
+
+function divide() {
+  return (a / b);
+}
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--
+}
